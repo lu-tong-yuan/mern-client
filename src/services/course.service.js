12a640f80215eaaf3ec755f3597b062c201170c8
Tommy <karta1385993@gmail.com>
2023-09-05 21:11:32 +0800

    change API_URL

--- src/services/course.service.js
+++ src/services/course.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/courses";
+const API_URL = "https://mern-server-tr7k.onrender.com/api/courses";
 
 class CourseService {
   post(title, description, price) {
